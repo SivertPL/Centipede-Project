bdf34bc6f9ea062dad09721a8d9fe9dc2b74122e
SivertPL <kroppoloe@protonmail.cj>
2018-09-30 21:37:09 +0200

    2

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-byle co
+#!/usr/bin/python3
+import messages
 
-a to moja super aktualizacja
+
+"""
+    Plan projektu:
+
+        1. Spider 
+        - Pająk który zbiera wszystkie pliki ze strony:
+        docx na początek
+        2. Analyzer
+        - Analizuje zebrane pliki i wyciaga z nich metadane
+
+
+        Na razie tylko format .docx
+"""
+
+def main():
+    messages.info("Test info by sebix")
+    messages.warning("Test warning by sebix")
+    messages.error("TEST error by sebix")
+    
+
+
+if __name__ == "__main__":
+    main()
