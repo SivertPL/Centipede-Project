a460703095bb9756dec59a3329740f2b91e5a6a0
Patryk <kroppoloe@protonmail.ch>
2018-10-10 21:53:40 +0200

    Cleanup & minor

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Centipede-Project
+## WIP collab SivertPL & PatientWidar
--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python3
 import messages
-import spider.spider
+import spider.spider as spdr 
 
-"""
-    Plan projektu:
-
-        1. Spider 
-        - Pająk który zbiera wszystkie pliki ze strony:
-        docx na początek
-        2. Analyzer
-        - Analizuje zebrane pliki i wyciaga z nich metadane
-
-
-        Na razie tylko format .docx
-
-"""
 
 def main():
-    messages.info("Test info by sebix")
-    messages.warning("Test warning by sebix")
-    messages.error("TEST error by sebix")
-    spider.spider.findimages("http://page.art.pl")
+    messages.info("spider test")
+    spdr.findimages("http://page.art.pl")
     
-
 
 if __name__ == "__main__":
     main()
