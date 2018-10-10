4bb3ccc9b54652cbe1bd3b435c7482049281290a
Patryk <kroppoloe@protonmail.ch>
2018-10-10 22:12:44 +0200

    docx

--- analyzer/docx.py
+++ analyzer/docx.py
@@ -1,2 +1,25 @@
-def parse(a):
-	f = open(a,"r")
+## UNZIP DOCX & METADATA READ 
+import zipfile
+import os 
+
+def mktmp():
+	current_path = os.path.dirname(os.path.realpath(__file__))
+	directory = os.path.join(current_path, "tmp")
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	return directory
+
+
+def unzip(f):
+	dir = mktmp()
+	zip_ref = zipfile.ZipFile(f, 'r')
+	zip_ref.extractall(dir)
+	zip_ref.close()
+
+def parse(f):
+	tmp = mktmp()
+	# IMPORT XML MINIDOM API & PARSE METADATA
+
+
+
+
